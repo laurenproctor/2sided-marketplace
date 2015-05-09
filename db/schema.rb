ba9606e892b406ebd56e7509f9634b1095bc3795
Lauren Proctor <laurenproctor32@gmail.com>
2015-05-08 20:03:18 -0400

    Adding books and redirecting logged in users

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502145606) do
+ActiveRecord::Schema.define(version: 20150508235806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string   "name"
+    t.string   "author"
+    t.text     "description"
+    t.integer  "price"
+    t.boolean  "availability"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
